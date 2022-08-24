0f7ba82a55eedbdd4df701472e65bd6274c1ebfd
xDysar <lucasbnicolas@gmail.com>
2022-08-24 16:36:57 -0300

    Primer Commit V1

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import NuevoPresupuesto from "./NuevoPresupuesto";
+import ControlPresupuesto from "./ControlPresupuesto";
+
+const Header = ({
+  gastos,
+  setGastos,
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+}) => {
+  return (
+    <header>
+      <h1>Planificador de Gastos</h1>
+
+      {isValidPresupuesto ? (
+        <ControlPresupuesto
+            gastos={gastos}
+            setGastos={setGastos}
+            presupuesto={presupuesto}
+            setPresupuesto={setPresupuesto}
+            setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      ) : (
+        <NuevoPresupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      )}
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Modal.jsx
@@ -0,0 +1,116 @@
+import { useState, useEffect } from "react";
+import Mensaje from "./Mensaje";
+import CerrarBtn from "../img/cerrar.svg";
+
+const Modal = ({
+  setModal,
+  animarModal,
+  setAnimarModal,
+  guardarGasto,
+  gastoEditar,
+  setGastoEditar
+}) => {
+  const [mensaje, setMensaje] = useState("");
+
+  const [nombre, setNombre] = useState("");
+  const [cantidad, setCantidad] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [id, setId] = useState("");
+
+  useEffect(() => {
+    if (Object.keys(gastoEditar).length > 0) {
+      setNombre(gastoEditar.nombre);
+      setCantidad(gastoEditar.cantidad);
+      setCategoria(gastoEditar.categoria);
+      setFecha(gastoEditar.fecha);
+      setId(gastoEditar.id);
+    }
+  }, []);
+
+  const ocultarModal = () => {
+    setAnimarModal(false);
+    setGastoEditar({});
+    setTimeout(() => {
+      setModal(false);
+    }, 500);
+  };
+
+  const hundleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([nombre, cantidad, categoria].includes("")) {
+      setMensaje("Todos los campos son obligatorios");
+
+      setTimeout(() => {
+        setMensaje("");
+      }, 3000);
+      return;
+    }
+
+    guardarGasto({ nombre, cantidad, categoria, fecha, id });
+  };
+
+  return (
+    <div className="modal">
+      <div className="cerrar-modal">
+        <img src={CerrarBtn} alt="cerrar modal" onClick={ocultarModal} />
+      </div>
+
+      <form
+        onSubmit={hundleSubmit}
+        className={`formulario ${animarModal ? "animar" : "cerrar"}`}
+      >
+        <legend>{gastoEditar.nombre ? "Editar gasto" : "Nuevo gasto"}</legend>
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+          <input
+            id="nombre"
+            type="text"
+            placeholder="Añade el Nombre del Gasto"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+          <input
+            id="cantidad"
+            type="number"
+            placeholder="Añade la Cantidad del Gasto: ej. 300"
+            value={cantidad}
+            onChange={(e) => setCantidad(Number(e.target.value))}
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="categoria">Categoria</label>
+          <select
+            id="categoria"
+            value={categoria}
+            onChange={(e) => setCategoria(e.target.value)}
+          >
+            <option value="">-- Seleccione --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
+
+        <input
+          type="submit"
+          value={gastoEditar.nombre ? "Guardar Cambios" : "Añadir Gasto"}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Modal;
